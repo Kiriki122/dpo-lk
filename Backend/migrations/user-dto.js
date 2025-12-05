3880e9b4f554b822b53a6deb662098d57a37547c
Kiriki122 <kiriki496@gmail.com>
2025-12-05 22:31:06 +0300

    Моделе User на backend-е добавлены поля фамилия, имя, отчество

--- Backend/migrations/user-dto.js
+++ Backend/migrations/user-dto.js
@@ -1,11 +1,17 @@
 module.exports = class UserDto {
   email;
   phone;
+  firstName;
+  lastName;
+  middleName;
   id;
 
   constructor(model) {
     this.email = model.email;
     this.phone = model.phone;
+    this.firstName = model.firstName;
+    this.lastName = model.lastName;
+    this.middleName = model.middleName;
     this.id = model.id;
   }
 };
--- Backend/models/user.js
+++ Backend/models/user.js
@@ -9,6 +9,9 @@ module.exports = (sequelize, DataTypes) => {
   }
   User.init(
     {
+      firstName: { type: DataTypes.STRING, allowNull: false },
+      lastName: { type: DataTypes.STRING, allowNull: false },
+      middleName: { type: DataTypes.STRING, allowNull: true },
       email: { type: DataTypes.STRING, unique: true, allowNull: true },
       phone: { type: DataTypes.STRING, unique: true, allowNull: true },
       password: { type: DataTypes.STRING, allowNull: false },
