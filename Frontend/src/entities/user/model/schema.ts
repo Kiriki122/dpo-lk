c70aabf76df3e072cd6718d445295a7725babd3b
Kiriki122 <kiriki496@gmail.com>
2025-12-05 22:33:10 +0300

    Обновление модели User на fronend-е

--- Frontend/src/entities/user/model/schema.ts
+++ Frontend/src/entities/user/model/schema.ts
@@ -3,7 +3,9 @@ import { z } from "zod";
 
 export const UserSchema = z.object({
   id: z.number(),
-  // name: z.string().min(2, "Имя должно содержать не менее 2 символов"),
+  firstName: z.string().min(2, "Имя должно содержать не менее 2 символов"),
+  lastName: z.string().min(2, "Фамилия должна содержать не менее 2 символов"),
+  middleName: z.string().optional(),
   email: z.string().email({ pattern: z.regexes.email, error: "Некоректый email" }),
   phone: z.string().refine((v) => validator.isMobilePhone(v, "ru-RU"), { message: "invalid" }),
 });
