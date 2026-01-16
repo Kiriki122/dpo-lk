668c255983a8656a8f89a75a8633bcd973c78c5f
Kiriki122 <kiriki496@gmail.com>
2026-01-16 20:46:07 +0300

    Изменил компонент header-а для отображения именни и аватара пользоватля. Теперь он сслыка на страницу /profile

--- Frontend/src/shared/ui/Header/NameWithAvatar.tsx
+++ Frontend/src/shared/ui/Header/NameWithAvatar.tsx
@@ -1,12 +1,18 @@
-import { Avatar, Stack, Typography } from "@mui/material";
+import { Avatar, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import { pathKeys } from "@/shared/config/routes";
 
 export const NameWithAvatar = ({ name }: { name?: string }) => {
   return (
-    <Stack direction="row" spacing={2} alignItems="center">
-      <Typography variant="subtitle1" noWrap>
+    <Link
+      to={pathKeys.profile}
+      style={{ display: "flex", alignItems: "center", color: "inherit", textDecoration: "none" }}
+    >
+      <Typography variant="subtitle1" noWrap sx={{ mr: 2 }}>
         {name || "User"}
       </Typography>
       <Avatar alt={name || "User"}> {name?.charAt(0) || "U"}</Avatar>
-    </Stack>
+    </Link>
   );
 };
