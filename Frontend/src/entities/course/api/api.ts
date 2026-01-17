f2219c53af9ed4eb772d7b33a7396647cf64dddd
Kiriki122 <kiriki496@gmail.com>
2026-01-17 21:17:59 +0300

    Переделал courseApi из класса в объект

--- Frontend/src/entities/course/api/api.ts
+++ Frontend/src/entities/course/api/api.ts
@@ -1,16 +1,13 @@
 import { privateApi } from "@/shared/api/instance";
 import { CourseSchema, type Course } from "../model/types";
 
-class CourseApi {
-  async getAll() {
+export const courseApi = {
+  getAll: async () => {
     const response = await privateApi.get<Course[]>("/courses");
     return CourseSchema.array().parse(response.data);
-  }
-
-  async getById(id: number | string): Promise<Course> {
+  },
+  getById: async (id: number | string): Promise<Course> => {
     const response = await privateApi.get<Course>(`/courses/${id}`);
     return CourseSchema.parse(response.data);
-  }
-}
-
-export const courseApi = new CourseApi();
+  },
+};
