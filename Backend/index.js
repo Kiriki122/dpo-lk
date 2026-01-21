388d7176f4a660c9e059e1e48c1197c52913daa9
Kiriki122 <kiriki496@gmail.com>
2026-01-21 22:42:21 +0300

    Убрал cors т.к. теперь что dev что prod работают через reverse_proxy

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
 const db = require("./models");
 const router = require("./router");
 const errorMiddleware = require("./middlewares/error-middleware");
@@ -15,12 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    credentials: true,
-    origin: process.env.CLIENT_URL,
-  })
-);
+
 app.use("/api", router);
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
