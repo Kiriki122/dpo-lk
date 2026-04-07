8d2e7c6b723419e346651acb73f7b22053f35df1
Kiriki122 <kiriki496@gmail.com>
2026-04-08 01:40:27 +0300

    обновил prettier и eslint конфиги

--- Frontend/eslint.config.ts
+++ Frontend/eslint.config.ts
@@ -1,29 +1,34 @@
-import js from "@eslint/js";
-import globals from "globals";
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import importPlugin from "eslint-plugin-import";
+import pluginReact from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
 import tseslint from "typescript-eslint";
-import { defineConfig, globalIgnores } from "eslint/config";
-import importPlugin from "eslint-plugin-import";
 
 export default defineConfig([
-  globalIgnores(["dist"]),
   {
-    files: ["**/*.{ts,tsx}"],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs["recommended-latest"],
-      reactRefresh.configs.vite,
-    ],
-    plugins: {
-      import: importPlugin,
-    },
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    ignores: ["dist", "node_modules", ".dependency-cruiser.cjs", "public"],
+  },
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  reactHooks.configs["recommended-latest"],
+  reactRefresh.configs.vite,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { import: importPlugin },
+    languageOptions: { ecmaVersion: 2020, globals: globals.browser },
+    settings: {
+      react: {
+        version: "detect",
+        runtime: "automatic",
+      },
     },
     rules: {
+      "react/react-in-jsx-scope": "off",
       "import/order": [
         "error",
         {
@@ -68,4 +73,5 @@ export default defineConfig([
       ],
     },
   },
+  eslintConfigPrettier,
 ]);
